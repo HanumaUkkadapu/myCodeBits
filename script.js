be05c35b69f2cea42f3e6831dc72dc3f59204b2d
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2020-01-10 00:47:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,14 @@
   generated on: 10-01-2020
 */
 //var GitHub = require('github-api');
-import GitHub from 'github-api';
-
+/**************************************************************
 // basic auth
 var gh = new GitHub({
    /*
    username: 'FOO',
    password: 'NotFoo'
-   */
    /* also acceptable:
       token: 'MY_OAUTH_TOKEN'
-    */
 	token: '178e972ba461b3efce2d9abe5e787a5f8026bc8d'
 });
 
@@ -28,9 +25,31 @@ me.listNotifications(function(err, notifications) {
 	}
 });
 
-/*
 var clayreimann = gh.getUser('clayreimann');
 clayreimann.listStarredRepos(function(err, repos) {
    // look at all the starred repos!
 });
-*/
+**************************************************************8*/
+
+import GitHub from 'github-api';
+
+// unauthenticated client
+const gh = new GitHub();
+let gist = gh.getGist(); // not a gist yet
+gist.create({
+	public: true,
+	description: 'My first gist',
+	files: {
+	  "file1.txt": {
+		 content: "Aren't gists great!"
+	  }
+	}
+}).then(function({data}) {
+	// Promises!
+	let createdGist = data;
+	return gist.read();
+}).then(function({data}) {
+	let retrievedGist = data;
+	console.log(retrievedGist);
+	// do interesting things
+});
