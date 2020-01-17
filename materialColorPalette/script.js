478b69ef17540077fec84543df06dc2909b8adac
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2020-01-17 18:45:40 +0530

    Update script.js

--- materialColorPalette/script.js
+++ materialColorPalette/script.js
@@ -18,22 +18,22 @@ function JSONloaded(data){
 	const output1 = document.getElementById('out1');
 
 	const colorsObj = data;
-	//console.log(colorsObj.red["100"]);
 	const colorNames = Object.keys(colorsObj);
 	const shades = Object.keys(colorsObj[colorNames[0]]);
+	//console.log(colorsObj.red["100"]);
 
 	// setting select option values
 	for(var i=0;i<shades.length;i++){
 		var option = '<option value="'+shades[i]+'" >'+shades[i].toUpperCase()+'</option>';
 		optSel.insertAdjacentHTML('beforeend', option);
-		if(i==shades.length - 1)	setSelVal();
+		if(i==shades.length - 1){
+			setSelVal();
+		}
 	}
 
-	//
 	function setSelVal(){
 		optSel.value = 500;
 		changeShades(optSel.value);
-		colorClick();
 	}
 
 	// changing color palette on selecting a tone
@@ -49,7 +49,6 @@ function JSONloaded(data){
 				//console.log(toneVal+' exists in '+colorNames[i]);
 			}
 		}
-		colorClick();
 	}
 	// removing object's child nodes
 	function clear(obj){
@@ -64,6 +63,7 @@ function JSONloaded(data){
 		var colorBox = document.createElement('div');
 		var colorNameSpan = document.createElement('span');
 		colorBox.className = 'color-box';
+		colorBox.setAttribute("onclick","colorClick(this.id);");
 		colorNameSpan.className = 'color-name';
 		colorBox.append(colorNameSpan);
 		colorBox.style.background = bgColor;
@@ -73,25 +73,18 @@ function JSONloaded(data){
 		//console.log(bgColor+'\t'+colorNameCaps+'\n');
 	}
 
-	function colorClick(){
-		//NodeList.prototype.forEach = Array.prototype.forEach;
-		var paletteChild = [...palette.childNodes];
-		//console.log(typeof paletteChild+'\t'+paletteChild.length);
-		paletteChild.forEach( function(elem){
-			elem.addEventListener( "click", function(){
-				var colorNo = colorNames.indexOf(elem.id);
-				//console.log(elem.id+'\t'+colorNo);
-				var colorSel = colorsObj[elem.id][optSel.value];
-				document.body.style.background = colorSel;
-				var rgb = ['','',''];
-				for(i=1;i<7;i++){
-					if(i<3)	rgb[0] = rgb[0].concat(colorSel[i]);
-					else if(i>2&&i<5)	rgb[1] = rgb[1].concat(colorSel[i]);
-					else if(i>4&&i<7)	rgb[2] = rgb[2].concat(colorSel[i]);
-				}
-				//console.log(colorSel+'\t'+rgb[0]+','+rgb[1]+','+rgb[2]);
-				output1.innerHTML = '#'+rgb[0]+rgb[1]+rgb[2];//colorsObj[elem.id][optSel.value];
-			});
-		});
+	function colorClick(elemId){
+		//var colorNo = colorNames.indexOf(elemId);
+		//console.log(elem.id+'\t'+colorNo);
+		var colorSel = colorsObj[elemId][optSel.value];
+		document.body.style.background = colorSel;
+		var rgb = ['','',''];
+		for(i=1;i<7;i++){
+			if(i<3)	rgb[0] = rgb[0].concat(colorSel[i]);
+			else if(i>2&&i<5)	rgb[1] = rgb[1].concat(colorSel[i]);
+			else if(i>4&&i<7)	rgb[2] = rgb[2].concat(colorSel[i]);
+		}
+		//console.log(colorSel+'\t'+rgb[0]+','+rgb[1]+','+rgb[2]);
+		output1.innerHTML = '#'+rgb[0]+rgb[1]+rgb[2];//colorsObj[elem.id][optSel.value];
 	}
 }
